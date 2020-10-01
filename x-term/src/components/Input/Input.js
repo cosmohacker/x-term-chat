4139cac493516de378c6892eca10ace105a8e768
Yağızcan Yavuz <yagizcanyavuz@outlook.com>
2020-10-01 03:34:26 +0300

    Add files via upload

---
+++ x-term/src/components/Input/Input.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Input.css';
+
+const Input = ({ setMessage, sendMessage, message }) => (
+  <form className="form">
+    <input
+      className="input"
+      type="text"
+      placeholder="Mesaj Yaz..."
+      value={message}
+      onChange={({ target: { value } }) => setMessage(value)}
+      onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+    />
+    <button className="sendButton" onClick={e => sendMessage(e)}>Gönder</button>
+  </form>
+)
+
+export default Input;
